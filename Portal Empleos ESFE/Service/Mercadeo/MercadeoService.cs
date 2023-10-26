90ad074303996859a51cf167b86686a1d703eb61
Osmany Lopez <osmany203lopez@gmail.com>
2023-10-25 20:54:34 -0600

    creacion de la clase de service Mercadeo

---
+++ Portal Empleos ESFE/Service/Mercadeo/MercadeoService.cs
@@ -0,0 +1,24 @@
+﻿using Portal_Empleos_ESFE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Portal_Empleos_ESFE.Service.Mercadeo
+{
+    internal class MercadeoService : IMercadeoService
+    {
+        const string URL = "https://portal-empleo-api.onrender.com/empleosMercadeo";
+
+        public async Task<List<MercadeoModel>> Get()
+        {
+            var httpClient = new HttpClient();
+            var reponse = await httpClient.GetAsync(URL);
+            var content = await reponse.Content.ReadAsStringAsync();
+            var mercadeoModel = JsonSerializer.Deserialize<List<MercadeoModel>>(content);
+            return mercadeoModel;
+        }
+    }
+}
