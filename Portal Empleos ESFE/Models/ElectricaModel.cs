7fb121ceeb449ca53095f7ac4cb6286af6b45fb5
Osmany Lopez <osmany203lopez@gmail.com>
2023-10-25 20:51:13 -0600

    Creacion de los modelos para cada carrera

---
+++ Portal Empleos ESFE/Models/ElectricaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal_Empleos_ESFE.Models
+{
+    internal class ElectricaModel
+    {
+        public int id { get; set; }
+        public string Titulo { get; set; }
+        public string Imagen { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
