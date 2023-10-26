689c20a6cb55424d8967be57e313e00814a31d2b
Osmany Lopez <osmany203lopez@gmail.com>
2023-10-25 20:55:21 -0600

    creacion de la clase de la interfaz Software

---
+++ Portal Empleos ESFE/Service/Software/ISoftwareService.cs
@@ -0,0 +1,14 @@
+﻿using Portal_Empleos_ESFE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal_Empleos_ESFE.Service.Software
+{
+    internal interface ISoftwareService
+    {
+        public Task<List<SoftwareModel>> Get();
+    }
+}
