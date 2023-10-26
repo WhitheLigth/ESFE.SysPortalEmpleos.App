fb8586e8b9a09ad97b57a3bc90947e47b48dd90c
Osmany Lopez <osmany203lopez@gmail.com>
2023-10-25 20:54:25 -0600

    creacion de la clase de service Software

---
+++ Portal Empleos ESFE/Service/Software/SoftwareService.cs
@@ -0,0 +1,24 @@
+﻿using Portal_Empleos_ESFE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Portal_Empleos_ESFE.Service.Software
+{
+    internal class SoftwareService : ISoftwareService
+    {
+        const string URL = "https://portal-empleo-api.onrender.com/empleoSoftware";
+
+        public async Task<List<SoftwareModel>> Get()
+        {
+            var httpClient = new HttpClient();
+            var reponse = await httpClient.GetAsync(URL);
+            var content = await reponse.Content.ReadAsStringAsync();
+            var softwareModel = JsonSerializer.Deserialize<List<SoftwareModel>>(content);
+            return softwareModel;
+        }
+    }
+}
