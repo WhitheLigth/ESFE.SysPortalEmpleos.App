38caa7f80480910e43e9a48a785ba9320f801aff
Osmany Lopez <osmany203lopez@gmail.com>
2023-10-25 20:55:09 -0600

    creacion de la clase de la interfaz Mercadeo

---
+++ Portal Empleos ESFE/Service/Mercadeo/IMercadeoService.cs
@@ -0,0 +1,14 @@
+﻿using Portal_Empleos_ESFE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal_Empleos_ESFE.Service.Mercadeo
+{
+    internal interface IMercadeoService
+    {
+        public Task<List<MercadeoModel>> Get();
+    }
+}
