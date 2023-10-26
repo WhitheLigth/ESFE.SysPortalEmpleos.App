81ba3f54c1adc97048844b35191f0a1784a34bf5
Osmany Lopez <osmany203lopez@gmail.com>
2023-10-25 20:54:57 -0600

    creacion de la clase de la interfaz Electrica

---
+++ Portal Empleos ESFE/Service/Electrica/IElectricaService.cs
@@ -0,0 +1,14 @@
+﻿using Portal_Empleos_ESFE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal_Empleos_ESFE.Service.Electrica
+{
+    internal interface IElectricaService
+    {
+        public Task<List<ElectricaModel>> Get();
+    }
+}
