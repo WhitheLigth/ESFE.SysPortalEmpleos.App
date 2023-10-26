be6c8dd327882123fdc5c62d164deac62db6b963
Osmany Lopez <osmany203lopez@gmail.com>
2023-10-25 20:56:08 -0600

    Se agregaron los builder para cada carrera

--- Portal Empleos ESFE/MauiProgram.cs
+++ Portal Empleos ESFE/MauiProgram.cs
@@ -1,5 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Portal_Empleos_ESFE.Service;
+
+//USING AGREGADOS
 using Portal_Empleos_ESFE.Data;
+using Portal_Empleos_ESFE.Service.Mercadeo;
+using Portal_Empleos_ESFE.Service.Electrica;
+using Portal_Empleos_ESFE.Service.Software;
+using Portal_Empleos_ESFE.Service.Turismo;
 
 namespace Portal_Empleos_ESFE
 {
@@ -16,8 +23,12 @@ namespace Portal_Empleos_ESFE
 				});
 
 			builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddSingleton<IMercadeoService, MercadeoService>();
+            builder.Services.AddSingleton<ISoftwareService, SoftwareService>();
+            builder.Services.AddSingleton<IElectricaService, ElectricaService>();
+            builder.Services.AddSingleton<ITurismoService, TurismoService>();
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
 
 			builder.Services.AddSingleton<WeatherForecastService>();
