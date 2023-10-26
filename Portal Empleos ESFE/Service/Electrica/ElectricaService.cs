0f7dbb2d92bf275d27f17e5e3b9379edcc650ac8
Osmany Lopez <osmany203lopez@gmail.com>
2023-10-25 20:54:01 -0600

    creacion de la clase de service Electrica

---
+++ Portal Empleos ESFE/Service/Electrica/ElectricaService.cs
@@ -0,0 +1,23 @@
+﻿using Portal_Empleos_ESFE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Portal_Empleos_ESFE.Service.Electrica
+{
+    internal class ElectricaService : IElectricaService
+    {
+        const string URL = "https://portal-empleo-api.onrender.com/empleosElectrica";
+        public async Task<List<ElectricaModel>> Get()
+        {
+            var httpClient = new HttpClient();
+            var reponse = await httpClient.GetAsync(URL);
+            var content = await reponse.Content.ReadAsStringAsync();
+            var electricaModel = JsonSerializer.Deserialize<List<ElectricaModel>>(content);
+            return electricaModel;
+        }
+    }
+}
